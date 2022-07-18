4b339d45008b9cd3c09a93011cb2c9b035836b3d
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-18 14:52:02 +0300

    remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import {
   persistStore,
   persistReducer,
@@ -19,7 +19,7 @@ const middleware = [
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
     },
   }),
-  logger,
+//   logger,
 ];
 
 const contactsPersistConfig = {
